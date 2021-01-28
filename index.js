cf8ab06bcf5763a05ba7421a6acf595d4fe0cd3c
MattBrwn <matthias_braun@freenet.de>
2021-01-28 14:50:02 +0100

    more styling

--- index.js
+++ index.js
@@ -1,61 +1,76 @@
+// Declaration of Canvas
 let canvas = document.querySelector('canvas')
 canvas.style.backgroundColor = "white"
 let ctx = canvas.getContext('2d')
 // ctx.fillRect(10, 10, 780, 430);
 canvas.style.border = "7px solid #77a352";
+
 //Fetch body
 let body = document.querySelector('body')
 
+// Declaration of Variables
 let charliebrownX = 20 + 550 * Math.random();
 let charliebrownY = 2;
+let charliebrownImg = new Image();
+    charliebrownImg.src = './images/CBrown.png'
 
 let ballX = charliebrownX + 40;
 let ballY = charliebrownY + 48;
 let ballWidth = 20;
-let ballincrementX = (Math.round(Math.random(1)) * 2 - 1) * (2 + (Math.random(1.5)));
-let ballincrementY = 2.5 + Math.random(1);
+let ballincrementX = (Math.round(Math.random(1)) * 2 - 1) * (1 + (Math.random(0.8)));
+let ballincrementY = 2 + Math.random(1);
+let numberOfBalls = 1
+let ballImg = new Image();
+    ballImg.src = './images/ball.PNG'
+let numbersofLostballs = 0;
+let numberofWonballs = 0;
 
-let snoopyX = 400 * Math.random();
-let snoopyY = 735;
+let snoopyX = 100 + (canvas.width-150) * Math.random();
+let snoopyY = (canvas.height-65);
 let snoopyWidth = 60;
 let incrementxSnoopy = 5;
+let snoopyImg = new Image();
+    snoopyImg.src = './images/SnoopyRunningLeft.png'
+// let kickingSound = document.getElementById('sound')
 
 let isLeftArrow = false;
 let isRightArrow = false;
 let score = 0;
 let intervalID= 0;
 
-let charliebrownImg = new Image();
-charliebrownImg.src = './images/CBrown.png'
 
-let ballImg = new Image();
-ballImg.src = './images/ball_.PNG'
-
-let snoopyImg = new Image();
-    snoopyImg.src = './images/Kopie von Snoopy running left.png'
-
-
-// let kickingSound = document.getElementById('sound')
-
-// Def different screens for transition
+// Def screens for transition
 let startscreen = document.getElementById('startscreen')
 let gameOverscreen = document.getElementById('gameoverscreen')
 let gameWinscreen = document.getElementById('gamewinscreen')
 
-
+// attributing buttons to DOM
 let startBtn = document.querySelector('#startgame')
 let restartBtnLose = document.querySelector('#restartgamelose')
 let restartBtnWin = document.querySelector('#restartgamewin')
 
+// Buttonevents
+restartBtnLose.addEventListener('click', ()=>{
+    restartgame()
+})
+
+restartBtnWin.addEventListener('click', ()=>{
+    restartgame()
+})
+
+
+
+
+// attributing keys for Snoopy movement
 document.addEventListener('keydown', (event) => {
     
     if (event.keyCode == 39 ||event.key == "ArrowRight") {
-        //snoopyImg.src = './images/snoopy running right.png'
+        snoopyImg.src = './images/SnoopyRunning Right.png'
         isRightArrow = true;
         isLeftArrow = false;
      }
      else if (event.keyCode == 37 || event.key == "ArrowLeft") {
-        //snoopyImg.src = './images/Kopie von Snoopy running left.png'
+        snoopyImg.src = './images/SnoopyRunningLeft.png'
         isRightArrow = false;
         isLeftArrow = true;
      }
@@ -67,33 +82,22 @@ document.addEventListener('keyup', (event) => {
 })
 
 
-restartBtnLose.addEventListener('click', ()=>{
-    //Setting everything back to normal with a function
-    restartgame()
-    console.log('Works')
-    
-})
 
-restartBtnWin.addEventListener('click', ()=>{
-    //Setting everzthing back to 0 with a function
-    console.log('Works')
-    restartgame()
-})
 
 function ballCollision(){
     
-    // check for right
+    // check for right side
     if (ballX+20 > canvas.width) {
         // change balldirection
         ballincrementX = (-1.2)*ballincrementX;
     }  
     
-    //check for left
+    //check for left side
     if (ballX < 0) {
         ballincrementX = (-1.5)*ballincrementX;
     }
     
-    //check for bottom
+    // check for bottom
     if (ballY >= canvas.height-40) {
         // something here 
         if (ballX + ballWidth > snoopyX && ballX > snoopyX + snoopyWidth || ballX < snoopyX + snoopyWidth && ballX +ballWidth < snoopyX) {
@@ -103,11 +107,12 @@ function ballCollision(){
         else {
             clearInterval(intervalID)
             gameWin()
+            }
         }
     }
 
     
-}
+
 
 function drawSnoopy(){
     ctx.drawImage(snoopyImg, snoopyX, snoopyY, 60, 60)
@@ -123,8 +128,6 @@ function drawBall(){
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height) 
-    ctx.font = '24px Verdana'
-    ctx.fillText('Score:' + score, 20, 20)
     drawCharliebrown()
     drawBall()
     drawSnoopy()
@@ -151,8 +154,6 @@ function startGame(){
     startscreen.style.display = "none"
     gameOverscreen.style.display = "none"
     gameWinscreen.style.display = "none"
-    
-    
     intervalID = setInterval(() => {
         requestAnimationFrame(draw)
     }, 10)
@@ -172,9 +173,6 @@ function gameOver(){
     */
     gameOverscreen.style.display = "block"
     gameWinscreen.style.display = "none"
-    
-
-
 }
 
 function restartgame(){
@@ -197,9 +195,10 @@ function restartgame(){
 
      isLeftArrow = false;
      isRightArrow = false;
-     score = 0;
+    //  score = 0;
      intervalID= 0;  
     startGame()
+    console.log(numberOfBalls)
 }
 
 
@@ -209,31 +208,12 @@ function gameWin(){
     startscreen.style.display = "none"
     gameOverscreen.style.display = "none"
     gameWinscreen.style.display = "block"
-    //restartBtn.style.display = 'block'
-    // intervalID = setInterval(() => {
-    //     requestAnimationFrame(draw)
-    // }, 10)
-
-  
-
 }
 
 
 // add your score text
 // ctx.font = '20px Verdana'
 // ctx.fillText('Score: ' + score, 20, 20)
-
-
-// window.addEventListener('load', () => {
-//     canvas.style.display = 'block'
-//     startscreen.style.display = "none"
-//     gameOverscreen.style.display = "none"
-
-//     restartBtn.addEventListener('click', () => {
-//         startGame()
-//     })
-
-// })
 
 window.addEventListener('load', () => {
     canvas.style.display = 'none'
